67bc42a476793c149ca80c1a342119213992eb7b
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-28 14:14:16 +0000

    final solution

--- index.js
+++ index.js
@@ -3,3 +3,37 @@ You are a professional robber planning to rob houses along a street. Each house 
 
 Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
 */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+  // let sum = 0
+  // for (let i=0; i<nums.length; i++){
+  //   if (i===0 | i % 2 ===0) sum += nums[i]
+  //   console.log(sum)
+  // }
+  // return sum
+  
+  
+    const rob = (nums, i) => {
+      if (i<0) return 0
+        // console.log(Math.max(rob(nums,i-2)+nums[i], rob(nums, i-1)))
+        return Math.max(rob(nums,i-2)+nums[i], rob(nums, i-1))
+    }
+    
+    return rob(nums, nums.length-1)
+};
+
+/*
+public int rob(int[] nums) {
+    return rob(nums, nums.length - 1);
+}
+private int rob(int[] nums, int i) {
+    if (i < 0) {
+        return 0;
+    }
+    return Math.max(rob(nums, i - 2) + nums[i], rob(nums, i - 1));
+}
+*/
